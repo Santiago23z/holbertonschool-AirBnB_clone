112191eb5d83dd69e9693a8f4cb2a8363f2c91a9
Santiago23z <santizapataalvares@gmail.com>
2022-07-04 11:06:49 -0500

    Commit update

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module for amenity class for AirBnB CLONE project - the console
+"""
+
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """Defines all instances attirbutes for an amenity instance
+    """
+    name = ""
