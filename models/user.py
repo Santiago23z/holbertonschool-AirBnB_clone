e176dae293df210f87e656988387a0cedeae5712
carlosberrio <carlosberriomon@yahoo.com>
2022-07-03 21:22:06 -0500

    State update

--- models/user.py
+++ models/user.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """module for user class AirBnb project"""
 
-import mailbox
-from ssl import _PasswordType
-from unicodedata import name
 from models.base_model import BaseModel
 
 class User(BaseModel):
