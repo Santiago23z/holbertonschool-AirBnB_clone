a4b120c0074181f9d2c687960c7a9f15e8bdcfee
carlosberrio <carlosberriomon@yahoo.com>
2022-07-05 20:36:22 -0500

    update classes

--- models/user.py
+++ models/user.py
@@ -2,15 +2,16 @@
 """module for user class AirBnb project"""
 
 from models.base_model import BaseModel
+import models
+
 
 class User(BaseModel):
     """defines all instance attributes for a User instance
-
         Public class attributes:
-        email <string>: User´s e-mailbox
+        email <string>: User's e-mailbox
         password <string>: User _Password
-        fisrt_name <string>: User´s firs name
-        last_name <string>: User´s last name
+        fisrt_name <string>: User's firs name
+        last_name <string>: User's last name
     """
     email = ""
     password = ""
