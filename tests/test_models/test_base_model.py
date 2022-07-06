174e9de843e8c4cc262340a9c80775e37bd1a0a7
carlosberrio <carlosberriomon@yahoo.com>
2022-07-06 16:41:26 -0500

    update test base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -20,7 +20,7 @@ class Test_Base_Model(unittest.TestCase):
         """
         self.assertIsInstance(self.object, BaseModel)
 
-    def attr(self):
+    def test_attr(self):
         """
         """
         object_string = str(self.object)
@@ -31,21 +31,13 @@ class Test_Base_Model(unittest.TestCase):
                 counter += 1
         self.assertEqual(counter, 3)
 
-    def arg_class(self):
+    def test_arg_class(self):
         """testing a class created with arguments"""
         class1 = BaseModel(__class__="test", id="123456789")
         self.assertEqual(type(class1), BaseModel)
 
-    def kawargs(self):
-        """testing kawargs"""
-        class2 = BaseModel(name="pepito", age=18)
-        test_dic = class2.to_dict()
-        attributes = ["name", "age", "__class__"]
-        attr_dic = list(test_dic.keys())
-        self.assertCountEqual(attr_dic, attributes)
-
     @mock.patch("models.storage")
-    def save(self, mock_engine):
+    def test_save(self, mock_engine):
         """testing the save method (updated dates)"""
         first_update = self.object.updated_at
         self.object.save()
@@ -53,15 +45,15 @@ class Test_Base_Model(unittest.TestCase):
         self.assertNotEqual(first_update, second_update)
         self.assertTrue(mock_engine.save.called)
 
-    def to_dict(self):
+    def test_to_dict(self):
         """testing the transform from object to dictionary"""
         self.object.name = "poncho"
         dict_object = self.object.to_dict()
         attributes = ["id", "name", "created_at", "updated_at", "__class__"]
-        new_attr = list(dict_object.keys)
+        new_attr = list(dict_object.keys())
         self.assertCountEqual(new_attr, attributes)
 
-    def value_dic(self):
+    def test_value_dic(self):
         """testing the values of the dictionary"""
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.object.name = "poncho"
@@ -70,7 +62,7 @@ class Test_Base_Model(unittest.TestCase):
         self.assertEqual(dict_object["created_at"],
                          self.object.created_at.strftime(time_format))
         self.assertEqual(dict_object["updated_at"],
-                         self.object.created_at.strftime(time_format))
+                         self.object.updated_at.strftime(time_format))
         self.assertEqual(dict_object["__class__"], "BaseModel")
 
     def test_str(self):
