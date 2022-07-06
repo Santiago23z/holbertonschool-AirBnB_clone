c9c5a68d73911ccfa7e3834bfd9453e31b1c3c21
carlosberrio <carlosberriomon@yahoo.com>
2022-07-05 20:47:39 -0500

    update base_model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
 """Test base Model"""
 
-from this import d
 import unittest
-
-from pip import main
 from models.base_model import BaseModel
 from unittest import mock
+
 
 class Test_Base_Model(unittest.TestCase):
     """
     """
+
     def setUp(self):
         """
         """
         self.object = BaseModel()
-        
+
     def test_init(self):
         """
         """
         self.assertIsInstance(self.object, BaseModel)
-        
+
     def attr(self):
         """
         """
@@ -34,17 +33,17 @@ class Test_Base_Model(unittest.TestCase):
 
     def arg_class(self):
         """testing a class created with arguments"""
-        class1 = BaseModel(__class__ = "test", id = "123456789")
+        class1 = BaseModel(__class__="test", id="123456789")
         self.assertEqual(type(class1), BaseModel)
-        
+
     def kawargs(self):
         """testing kawargs"""
-        class2 = BaseModel(name = "pepito", age = 18)
+        class2 = BaseModel(name="pepito", age=18)
         test_dic = class2.to_dict()
         attributes = ["name", "age", "__class__"]
         attr_dic = list(test_dic.keys())
         self.assertCountEqual(attr_dic, attributes)
-        
+
     @mock.patch("models.storage")
     def save(self, mock_engine):
         """testing the save method (updated dates)"""
@@ -53,7 +52,7 @@ class Test_Base_Model(unittest.TestCase):
         second_update = self.object.updated_at
         self.assertNotEqual(first_update, second_update)
         self.assertTrue(mock_engine.save.called)
-        
+
     def to_dict(self):
         """testing the transform from object to dictionary"""
         self.object.name = "poncho"
@@ -61,17 +60,19 @@ class Test_Base_Model(unittest.TestCase):
         attributes = ["id", "name", "created_at", "updated_at", "__class__"]
         new_attr = list(dict_object.keys)
         self.assertCountEqual(new_attr, attributes)
-        
+
     def value_dic(self):
         """testing the values of the dictionary"""
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.object.name = "poncho"
         dict_object = self.object.to_dict()
         self.assertEqual(dict_object["name"], "poncho")
-        self.assertEqual(dict_object["created_at"], self.object.created_at.strftime(time_format))
-        self.assertEqual(dict_object["updated_at"], self.object.created_at.strftime(time_format))
+        self.assertEqual(dict_object["created_at"],
+                         self.object.created_at.strftime(time_format))
+        self.assertEqual(dict_object["updated_at"],
+                         self.object.created_at.strftime(time_format))
         self.assertEqual(dict_object["__class__"], "BaseModel")
-        
+
     def test_str(self):
         """testing the string of the dictionary"""
         object_str = f"[BaseModel] ({self.object.id}) {self.object.__dict__}"
