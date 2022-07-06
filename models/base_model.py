798ef5935d4e682ab72231645f217a768b1a06be
carlosberrio <carlosberriomon@yahoo.com>
2022-07-05 20:40:59 -0500

    update base_model

--- models/base_model.py
+++ models/base_model.py
@@ -9,6 +9,7 @@ import uuid
 from datetime import datetime
 import models
 
+
 class BaseModel:
     """Class that define all common attributes/methods
     for other classes
@@ -19,9 +20,9 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
-                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 if key != '__class__':
-                        setattr(self, key, value)
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
