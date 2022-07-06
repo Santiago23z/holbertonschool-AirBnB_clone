f694e453ef2ca953442e137dd4fe01903ebe26fc
carlosberrio <carlosberriomon@yahoo.com>
2022-07-06 16:19:27 -0500

    update test city

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -4,15 +4,15 @@ Testing User
 """
 
 from datetime import datetime
-from models.state import State
+from models.city import City
 from models.base_model import BaseModel
 import unittest
 from models.engine.file_storage import FileStorage
 import os
 
 
-class TestUser(unittest.TestCase):
-    """Class test state"""
+class TestCity(unittest.TestCase):
+    """Class test city"""
 
     def setUp(self):
         """Method setup"""
@@ -31,15 +31,15 @@ class TestUser(unittest.TestCase):
 
     def test_instance(self):
         """test instance"""
-        new_object = State()
-        self.assertEqual(str(type(new_object)), "<class 'models.state.State'>")
-        self.assertIsInstance(new_object, State)
+        new_object = City()
+        self.assertEqual(str(type(new_object)), "<class 'models.city.City'>")
+        self.assertIsInstance(new_object, City)
         self.assertTrue(issubclass(type(new_object), BaseModel))
 
     def test_attr(self):
         """test attributes"""
-        attributes = {'name': str}
-        new_obj = State()
+        attributes = {'state_id': str, 'name': str}
+        new_obj = City()
         for k, value in attributes.items():
             self.assertTrue(hasattr(new_obj, k))
             self.assertEqual(type(getattr(new_obj, k, None)), value)
